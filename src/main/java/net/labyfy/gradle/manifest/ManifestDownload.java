3638af296d0b88a30d37e509f8cbe595afcb6ac1
Jan Brachthäuser <contact@jan-br.de>
2020-07-13 14:31:01 +0200

    Resolve very much "mimimimimimimi"

--- src/main/java/net/labyfy/gradle/manifest/ManifestDownload.java
+++ src/main/java/net/labyfy/gradle/manifest/ManifestDownload.java
@@ -4,15 +4,20 @@ import java.util.Objects;
 
 /**
  * Java representation for labyfy installer download entry for manifest file.
+ *
+ * @see ManifestDownloadData
  */
 public class ManifestDownload {
 
   private static final String type = "DOWNLOAD_FILE";
-  private ManifestDownloadData data = new ManifestDownloadData();
+  private final ManifestDownloadData data = new ManifestDownloadData();
 
   public ManifestDownload() {
   }
 
+  /**
+   * @see ManifestDownloadData
+   */
   public ManifestDownload(String url, String path, String md5) {
     this.data
         .setUrl(url)
@@ -20,28 +25,46 @@ public class ManifestDownload {
         .setMd5(md5);
   }
 
+  /**
+   * @see ManifestDownloadData#getUrl()
+   */
   public String getUrl() {
     return data.getUrl();
   }
 
+  /**
+   * @see ManifestDownloadData#setUrl(String)
+   */
   public ManifestDownload setUrl(String url) {
     data.setUrl(url);
     return this;
   }
 
+  /**
+   * @see ManifestDownloadData#getPath()
+   */
   public String getPath() {
     return data.getPath();
   }
 
+  /**
+   * @see ManifestDownloadData#setPath(String)
+   */
   public ManifestDownload setPath(String path) {
     data.setPath(path);
     return this;
   }
 
+  /**
+   * @see ManifestDownloadData#getMd5()
+   */
   public String getMd5() {
     return data.getMd5();
   }
 
+  /**
+   * @see ManifestDownloadData#setMd5(String)
+   */
   public ManifestDownload setMd5(String md5) {
     data.setMd5(md5);
     return this;
