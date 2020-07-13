518942edb5c227343a311d27abf354929052c635
Jan Brachthäuser <contact@jan-br.de>
2020-07-13 13:50:21 +0200

    Resolve "This class is missing documentation"

--- src/main/java/net/labyfy/gradle/publish/AssetPublisher.java
+++ src/main/java/net/labyfy/gradle/publish/AssetPublisher.java
@@ -13,18 +13,46 @@ import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
 
+/**
+ * Utility class to publish assets to https://git.laby.tech/client/labymod4/lm-distributor.
+ */
 public class AssetPublisher {
 
   private final String baseUrl;
 
+  /**
+   * Constructs an {@link AssetPublisher} that must be initialized with the base url of the distributor.
+   *
+   * @param baseUrl The publish url of the distributor
+   */
   public AssetPublisher(String baseUrl) {
     this.baseUrl = baseUrl;
   }
 
+  /**
+   * Publishes the content of a file to baseUrl/publish/{name}/{version}/{file.getName()}
+   * to the provided lm-distributor.
+   *
+   * @param file    The file to publish to the distributor
+   * @param name    The publish name to distribute
+   * @param version The publish version to distribute
+   * @param token   The Authorization Bearer token to publish to the distributor
+   * @throws FileNotFoundException If file does not exist
+   */
   public void publish(File file, String name, String version, String token) throws FileNotFoundException {
     this.publish(new FileInputStream(file), name, version, file.getName(), token);
   }
 
+  /**
+   * Publishes the content of a file to baseUrl/publish/{name}/{version}/{fileName}
+   * to the provided lm-distributor.
+   *
+   * @param inputStream The data to publish to the distributor
+   * @param name        The publish name to distribute
+   * @param version     The publish version to distribute
+   * @param fileName    The publish fileName to distribute
+   * @param token       The Authorization Bearer token to publish to the distributor
+   */
   public void publish(InputStream inputStream, String name, String version, String fileName, String token) {
     try (InputStream finalInputStream = inputStream) {
       byte[] data = new byte[finalInputStream.available()];
@@ -35,8 +63,17 @@ public class AssetPublisher {
     }
   }
 
+  /**
+   * Publishes the content of a file to baseUrl/publish/{name}/{version}/{fileName}
+   * to the provided lm-distributor.
+   *
+   * @param data     The data to publish to the distributor
+   * @param name     The publish name to distribute
+   * @param version  The publish version to distribute
+   * @param fileName The publish fileName to distribute
+   * @param token    The Authorization Bearer token to publish to the distributor
+   */
   public void publish(byte[] data, String name, String version, String fileName, String token) {
-
     try {
       HttpEntity entity =
           MultipartEntityBuilder.create()
