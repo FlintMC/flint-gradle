fe59fc004086f4c174c95364b61dfb7750bb7809
Jan Brachthäuser <contact@jan-br.de>
2020-07-13 14:01:05 +0200

    Resolve "This method is quite complex, consider adding comments within the implementation"

--- src/main/java/net/labyfy/gradle/manifest/ManifestGenerator.java
+++ src/main/java/net/labyfy/gradle/manifest/ManifestGenerator.java
@@ -37,6 +37,7 @@ public class ManifestGenerator {
    * @param project The gradle plroject to install task in
    */
   public void installManifestGenerateTask(Project project) {
+    //Creates manifest publish task
     project.getTasks().create("labyfyManifestPublish", task -> {
       task.setGroup("publish");
       task.dependsOn("jar");
@@ -45,12 +46,14 @@ public class ManifestGenerator {
             .setName(project.getName())
             .setVersion(project.getVersion().toString())
             .setDescription(project.getDescription())
+            //Collect all project dependencies with configuration 'labyfy' and add it to the manifest
             .setDownloads(this.collectDownloads(project).toArray(new ManifestDownload[]{}));
         String manifestContent = new GsonBuilder().setPrettyPrinting().create().toJson(manifest);
         this.labyfyGradlePlugin.getAssetPublisher().publish(manifestContent.getBytes(Charset.defaultCharset()), project.getName(), project.getVersion().toString(), "manifest.json", this.labyfyGradlePlugin.getExtension().getPublishToken());
       });
     });
 
+    //Does the same thing, but publishes the manifest with version 'latest' in another task
     project.getTasks().create("labyfyManifestLatestPublish", task -> {
       task.setGroup("publish");
       task.dependsOn("jar");
@@ -59,6 +62,7 @@ public class ManifestGenerator {
             .setName(project.getName())
             .setVersion(project.getVersion().toString())
             .setDescription(project.getDescription())
+            //Collect all project dependencies with configuration 'labyfy' and add it to the manifest
             .setDownloads(this.collectDownloads(project).toArray(new ManifestDownload[]{}));
         String manifestContent = new GsonBuilder().setPrettyPrinting().create().toJson(manifest);
         this.labyfyGradlePlugin.getAssetPublisher().publish(manifestContent.getBytes(Charset.defaultCharset()), project.getName(), "latest", "manifest.json", this.labyfyGradlePlugin.getExtension().getPublishToken());
@@ -95,8 +99,10 @@ public class ManifestGenerator {
    */
   public Collection<ManifestDownload> collectDependencies(Project project) {
     Collection<ManifestDownload> manifestDownloads = new HashSet<>();
+    //Retrieve all dependencies from configuration 'labyfy'
     project.getConfigurations().maybeCreate("labyfy").getDependencies().forEach(dependency -> {
 
+      //if dependency is a project dependency, get it from the distributor
       if (dependency instanceof ProjectDependency) {
         File jar = ((ProjectDependency) dependency)
             .getDependencyProject()
@@ -109,8 +115,9 @@ public class ManifestGenerator {
 
         try {
           if (jar.exists())
+            //add to manifest downloads
             manifestDownloads.add(new ManifestDownload()
-                .setUrl(labyfyGradlePlugin.getPublishBaseUrl() + "/package/" + dependency.getName() + "/" + dependency.getVersion().toString() + "/" + jar.getName())
+                .setUrl(labyfyGradlePlugin.getPublishBaseUrl() + "/package/" + dependency.getName() + "/" + dependency.getVersion() + "/" + jar.getName())
                 .setPath("Labyfy/packages/" + jar.getName())
                 .setMd5(DigestUtils.md5Hex(Files.readAllBytes(jar.toPath()))));
         } catch (IOException e) {
@@ -120,6 +127,7 @@ public class ManifestGenerator {
         return;
       }
 
+      //if project is a maven dependency get it from maven repository
       for (ArtifactRepository repository : project.getRepositories().getAsMap().values()) {
         try {
           String url = ((URI) DefaultGroovyMethods.getProperties(repository).get("url")).toURL().toExternalForm();
@@ -127,11 +135,13 @@ public class ManifestGenerator {
           MavenArtifact mavenArtifact = new MavenArtifact(dependency.getGroup(), dependency.getName(), dependency.getVersion());
           String path = remoteMavenRepository.buildArtifactPath(mavenArtifact, false);
 
+          //check if resource exists
           try (InputStream inStream = remoteMavenRepository.getArtifactStream(mavenArtifact)) {
             if (inStream != null) {
               byte[] data = new byte[inStream.available()];
               inStream.read(data);
 
+              //Add to manifest downloads
               manifestDownloads.add(new ManifestDownload()
                   .setUrl(url + (path.startsWith("/") ? path.substring(1) : path))
                   .setMd5(DigestUtils.md5Hex(data))
