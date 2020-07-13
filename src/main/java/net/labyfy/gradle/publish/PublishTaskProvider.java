bff86ef04835189235252c1d9722b8cbd8ac56b5
Jan Brachthäuser <contact@jan-br.de>
2020-07-13 13:52:47 +0200

    Resolve "This class is missing documentation"

--- src/main/java/net/labyfy/gradle/publish/PublishTaskProvider.java
+++ src/main/java/net/labyfy/gradle/publish/PublishTaskProvider.java
@@ -4,6 +4,11 @@ import org.gradle.api.Project;
 
 public class PublishTaskProvider {
 
+  /**
+   * Installs a gradle task to publish the current project to the lm-distributor.
+   *
+   * @param project The project to install the task on
+   */
   public void installPublishTask(Project project) {
     project.getTasks().create("labyfyPublish", task -> {
       task.setGroup("publish");
