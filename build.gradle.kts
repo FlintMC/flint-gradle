e98ef6669a75676f18715815258e2601c12ba333
Jan <contact@jan-br.de>
2020-11-06 19:40:59 +0100

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,20 +19,13 @@ fun RepositoryHandler.flintGradlePluginRepository() {
     }
 }
 
-fun RepositoryHandler.flintRepository() {
-    val labymediaMavenAuthToken: String? by project
-
+fun RepositoryHandler.flintRepository(){
     maven {
-        setUrl("https://git.laby.tech/api/v4/groups/2/-/packages/maven")
-        name = "Gitlab"
+        setUrl("http://dist.labymod.net/api/v1/maven/release")
+        name = "Flint"
         credentials(HttpHeaderCredentials::class) {
-            if (System.getenv().containsKey("CI_JOB_TOKEN")) {
-                name = "Job-Token"
-                value = System.getenv("CI_JOB_TOKEN")
-            } else {
-                name = "Private-Token"
-                value = labymediaMavenAuthToken
-            }
+            name = "Authorization"
+            value = "Bearer CbtTjzAOuDBr5QXcGnBc1MB3eIHxcZetnyHtdN76VpTNgbwAf87bzWPCntsXwj52"
         }
         authentication {
             create<HttpHeaderAuthentication>("header")
@@ -57,7 +50,7 @@ repositories {
 }
 
 // 10.0.0 as default, only relevant for local publishing
-version = System.getenv().getOrDefault("VERSION", "2.5.4")
+version = System.getenv().getOrDefault("VERSION", "2.5.5")
 
 dependencies {
     implementation(group = "com.fasterxml.jackson.core", name = "jackson-core", version = "2.11.1")
@@ -67,8 +60,8 @@ dependencies {
     implementation(group = "org.apache.httpcomponents", name = "httpclient", version = "4.5.12")
     implementation(group = "io.github.java-diff-utils", name = "java-diff-utils", version = "4.7")
     implementation(group = "com.fasterxml.jackson.core", name = "jackson-core", version = "2.12.0-rc1")
-    implementation(group = "net.flintmc.installer", name = "logic-implementation", version = "1.1.2")
-    implementation(group = "net.flintmc.installer", name = "logic", version = "1.1.2")
+    implementation(group = "net.flintmc.installer", name = "logic-implementation", version = "1.1.3")
+    implementation(group = "net.flintmc.installer", name = "logic", version = "1.1.3")
 }
 
 gradlePlugin {
