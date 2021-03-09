a33b3256158642412159b36d2461d86e8881fab0
Janrupf <business.janrupf@gmail.com>
2021-03-09 17:12:09 +0100

    Bump version to 2.9.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,7 +66,7 @@ repositories {
     mavenCentral()
 }
 
-version = System.getenv().getOrDefault("VERSION", "2.9.0")
+version = System.getenv().getOrDefault("VERSION", "2.9.1")
 
 dependencies {
     implementation(group = "com.fasterxml.jackson.core", name = "jackson-core", version = "2.11.1")
